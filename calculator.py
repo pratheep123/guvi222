64eb8cbaf31e301fcedbcf0dded090063dfb3ecc
pratheep123 <59172396+pratheep123@users.noreply.github.com>
2020-01-23 09:19:30 -0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+a=input("enter the add or sub or mul or div:")
+if a=="add":
+  e=int(input())
+  f=int(input())
+  def add(c,d):
+    s=e+f
+    print(s)
+  add(e,f)
+elif(a=="sub"):
+  e=int(input())
+  f=int(input())
+  def add(c,d):
+    s=e-f
+    print(s)
+  add(e,f)
+elif(a=="mul"):
+  e=int(input())
+  f=int(input())
+  def add(c,d):
+    s=e*f
+    print(s)
+  add(e,f)
+elif(a=="div"):
+  e=int(input())
+  f=int(input())
+  def add(c,d):
+    s=e//f
+    print(s)
+  add(e,f)
