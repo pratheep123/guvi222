f11ec24043fd802a41563e73d3e29b3f2f166fde
pratheep123 <59172396+pratheep123@users.noreply.github.com>
2020-01-27 10:06:50 -0800

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,25 @@
+f=open("s",'w+')
+
+b={}
+
+for i in range(0,2):
+
+    k=input("Enter the key:")
+
+    v=int(input("Enter the value:"))
+
+    b.update({k:v})
+
+f.write("%s\r\n"%(b))
+
+ch=input("Enter the ch:")
+
+f.write("the ch is %s\r\n"%(ch))
+
+if ch in b.keys():
+ 
+    print(b[ch])
+
+    f.write("the mark is %d"%(b[ch]))
+
+f.close()
