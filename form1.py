a1dc1a9938c66b326fc0393a931f54ce85aeabbe
pratheep123 <59172396+pratheep123@users.noreply.github.com>
2020-01-29 20:48:37 -0800

    Add files via upload

---
+++ form1.py
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <title>registration form</title>
+
+    </head>
+
+    <body>
+        <h1 style="background-color:Tomato;">Tomato</h1>
+<h1 style="background-color:Orange;">Orange</h1>
+<h1 style="background-color:DodgerBlue;">DodgerBlue</h1>
+
+        <a href="#f1">form</a>
+
+        <form id="f1">
+
+            <h2>REGISTRATION FROM</h2>
+
+            <input type="text" placeholder="user name"><br>
+
+            <input type="number" placeholder="age"><br>
+
+            <input type="date" placeholder="DOB"><br>
+
+            <select>
+
+                <option value="bsc">bsc</option>
+
+                <option value="be">be</option>
+
+                <option value="bcom">bcom</option>
+
+            </select><br>
+
+                <input type="radio" name="gender" value="male" check>male<br>
+
+                <input type="radio" name="gender" value="female" check>female<br>
+
+                <input type="radio" name="gender" value="other" check>other<br>
+
+                <input type="button" value="submit"><br>
+                <marquee>this the form</marquee>
+            <p><a href="#C4">Jump to Chapter 4</a></p>
+<p><a href="#C10">Jump to Chapter 10</a></p>
+
+<h2>Chapter 1</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 2</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 3</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2 id="C4">Chapter 4</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 5</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 6</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 7</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 8</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2>Chapter 9</h2>
+<p>This chapter explains ba bla bla</p>
+
+<h2 id="C10">Chapter 10</h2>
+<p>This chapter explains ba bla bla</p>
+    
+
+            <p>the list of unordered items</p>
+
+             <ul style="list-style-type:square">
+
+              <li>coffee</li>
+
+              <li>tea</li>
+
+              <li>boost</li>
+
+             </ul>
+
+            <p>the list of ordered list</p>
+ 
+             <ol type="i">
+
+              <li>coffee</li>
+
+              <li>tea</li>
+
+              <li>boost</li>
+
+             </ol>
+       </form>
+
+    </body>
+
+</html>
